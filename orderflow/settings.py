715f6d81c53c785adb92886c27b1ce424e619b6f
Vaibhav Mishra <vaibhavmishra.vvm.work@gmail.com>
2026-07-09 13:31:32 +0530

    updation

--- orderflow/settings.py
+++ orderflow/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-orderflow-dev-key-change-me"
 DEBUG = True
-ALLOWED_HOSTS = ["https://ecomm-order-flow-vsmashers.vercel.app/"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
